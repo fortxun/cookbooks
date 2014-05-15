06e932164af6ecd6625f9246349efb6da4655270
fortxun <ewen.fortune@gmail.com>
2014-05-15 16:03:31 +0200

    Commentalized
    Added some comments

--- ipmicfg/recipes/default.rb
+++ ipmicfg/recipes/default.rb
@@ -11,15 +11,18 @@
 ipmicfg_version = "1.14.3_20130725"
 ipmicfg_archive = "ipmicfg_#{ipmicfg_version}.zip"
 
+#Need unzip to open the ipmicfg archive
 package "unzip" do
   action :install
 end
 
+#Download the ipmicfg archive unless its cached
 remote_file "#{Chef::Config[:file_cache_path]}/#{ipmicfg_archive}" do
   source "ftp://ftp.supermicro.com/utility/IPMICFG/#{ipmicfg_archive}"
   action :create_if_missing
 end
 
+#Unzip the ipmicfg archive to /usr/local
 bash "build ipmicfg-#{ipmicfg_version}" do
   code <<-EOH
 unzip #{Chef::Config[:file_cache_path]}/#{ipmicfg_archive} -d /usr/local
@@ -28,7 +31,7 @@ EOH
   creates "/usr/local/ipmicfg-#{ipmicfg_version}/linux/64bit/ipmicfg-linux.x86_64"
 end
 
-# If the system has installed the OpenIPMI driver, enable the Linux IPMI driver
+#If the system has installed the OpenIPMI driver, enable the Linux IPMI driver
 execute "ipmi start" do
   command "/etc/init.d/ipmi start"
   ignore_failure true
