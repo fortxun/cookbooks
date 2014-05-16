45ac6fd644bc0cb75a2602b765bd8d638b7732b7
fortxun <ewen.fortune@gmail.com>
2014-05-17 00:01:46 +0200

    Initial commit
    Initial commit of filter support for user cookbook

---
+++ users/attributes/default.rb
@@ -0,0 +1,7 @@
+# This is a Chef attributes file. It can be used to specify default and override
+# attributes to be applied to nodes that run this cookbook.
+
+# Set a default name
+default[:users][:node_filter] = 'none'
+
+# For further information, see the Chef documentation (http://docs.opscode.com/essentials_cookbook_attribute_files.html).
